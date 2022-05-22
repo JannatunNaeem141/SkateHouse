913288a05c2ff179a26117dda13547e09d498ed6
jannatunnaeem141 <jannatunnaeem141@gmail.com>
2022-05-22 08:42:59 +0600

    firebase setup & env-local set done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Navbar from './Pages/Shared/Navbar';
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/blog' element={<Blogs></Blogs>}></Route>
       </Routes>
     </div>
   );
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -7,6 +7,7 @@ const Navbar = () => {
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/reviews'>Reviews</Link></li>
         <li><Link to='/contact'>Contact Us</Link></li>
+        <li><Link to='/blog'>Blogs</Link></li>
         <li><Link to='/login'>Login</Link></li>
     </>
 
